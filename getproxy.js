437bf3baf80a397931a803095242b7d68a232190
lelegoyeng <lelegoyengs@gmail.com>
2023-01-06 10:40:58 +0700

    update proxy

--- getproxy.js
+++ getproxy.js
@@ -1,69 +1,84 @@
 const puppeteer = require('puppeteer');
 const randomUseragent = require('random-useragent');
 const cheerio = require('cheerio');
+require('dotenv').config()
+const { MongoClient } = require("mongodb");
+
+const url = process.env.url;
+const client = new MongoClient(url);
+const dbName = "test";
 
 let barray = 7
 let tip = 1
-let tport = 2 
+let tport = 2
 
-  function one(){
-    return new Promise(async resolve => {
-        const url = "https://free-proxy-list.net/";
-        const randomAgent = randomUseragent.getRandom();
-        const browser = await puppeteer.launch({
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-            ],
-        //    headless: false
-        });
-        const context = await browser.createIncognitoBrowserContext();
-        const page = await context.newPage();
-        await page.setJavaScriptEnabled(true);
-        await page.setUserAgent(randomAgent);
-        await page.goto(url, { waituntil: 'domcontentloaded', timeout: 0 });
-    
-        const data = await page.evaluate(() => {
-            const tds = Array.from(document.querySelectorAll('table tr td'))
-            return tds.map(td => td.innerText)
-        
-        });
-    
-      
-      console.log(data)
-    
-        // console.log(data[barray+tip]) //1
-        // console.log(data[barray+tport]) //2 
-        // console.log(data[2]) 
-        // console.log(data[3])
-        // console.log(data[4])
-        // console.log(data[5])
-        // console.log(data[6])
-        // console.log(data[7])
-        // console.log(data[8])
-        // console.log(data[9]) 
-    
-        await browser.close();
-      resolve();
+function one() {
+  return new Promise(async resolve => {
+    const url = "https://free-proxy-list.net/";
+    const randomAgent = randomUseragent.getRandom();
+    const browser = await puppeteer.launch({
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ],
+      //    headless: false
     });
-  }
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+    await page.setJavaScriptEnabled(true);
+    await page.setUserAgent(randomAgent);
+    await page.goto(url, { waituntil: 'domcontentloaded', timeout: 0 });
+
+    const data = await page.evaluate(() => {
+      const tds = Array.from(document.querySelectorAll('table tr td'))
+      return tds.map(td => td.innerText)
+
+    });
+
+    //db connection
+    await client.connect();
+    const db = client.db(dbName);
+    const col = db.collection("proxy");
+
+    let personDocument = {
+      "ip": data[barray + tip],
+      "port": data[barray + tport],
+      "country": data[barray + tport + 2],
+
+    }
+
+    //db insert
+    const p = await col.insertOne(personDocument);
+    console.log(p.insertedId)
+
+
+    // console.log(data[barray+tip]) 
+    // console.log(data[barray+tport])
+    // console.log(data[barray+tport+2])
+
+
+    await browser.close();
+    resolve();
+  });
+}
 
 let level = 1
-let temp = 20
+// let temp = 2557
+let temp = 2557
 
-const run = (async ()=> {
+const run = (async () => {
   await Promise.all([one()])
-  .then(s=> {
-    //  console.log(barray)
-    setTimeout(()=> {
-      temp == 0 ? process.exit(0) : temp--
+    .then(s => {
+      //  console.log(barray)
+      setTimeout(() => {
+        temp == 0 ? process.exit(0) : temp--
         barray = barray + 7
         tip = tip + 1
         tport = tport + 1
         level++
         run()
-    },1000)
-  })
+      }, 1000)
+    })
 })
 
 run()
